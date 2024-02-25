72dba5be8c49c03ccba5dcf7d1034b748e15f9e8
Ricardo Santos <riccardhos@gmail.com>
2024-02-25 19:34:00 -0300

    Create js_treino_arrays.js

---
+++ JAVA-SCRIPT/JS_ATIVIDADES/js_treino_arrays.js
@@ -0,0 +1,17 @@
+
+/*
+    let numero = [1, 2, 3, 4, 5];
+    numero[9] = 6;
+    console.log(numero);
+
+*/
+
+let num = [10, 2, 7, 1, 9];
+//num.sort();
+num[5] = 50;
+console.log(num);
+console.log(num.length);
+console.log(`Meu vetor têm ${num.length} posições!`);
+console.log(`Esse é o tamanho da Matriz: ${num.length}`);
+console.log(`O elemento ${num.indexOf(7)} ocupa essa posição!`);
+
